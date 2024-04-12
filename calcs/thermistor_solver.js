dd814ee2f4ab08f54a6d5bad7f529061f214c0fd
TL;DR-Electronics <aardwolfdigital@gmail.com>
2024-04-12 01:45:56 +0100

    Adding Thermistor Solver (WIP)

---
+++ calcs/thermistor_solver.js
@@ -0,0 +1,14 @@
+function calculateNtcResistance() {
+    const B = parseFloat(document.getElementById("B").value);
+    const rated_resistance = parseFloat(document.getElementById("rated_resistance").value);
+    const rated_temperature = parseFloat(document.getElementById("rated_temperature").value);
+    const target_temperature = parseFloat(document.getElementById("target_temperature").value);
+    const K = 273.15;
+
+    var y = rated_resistance * Math.exp(B * (1/(target_temperature+K) - 1/(rated_temperature+K)));
+    
+    var container = document.getElementById("result");
+
+    container.innerHTML = Math.round(y)
+}
+
